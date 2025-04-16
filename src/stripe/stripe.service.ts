9309cc62f18edd182f13f53a09f0d61268cb8738
Syed-Ahsan-Naqvi <syedahsannaqvi@gmail.com>
2025-04-16 10:25:48 +0500

    update commit

--- src/stripe/stripe.service.ts
+++ src/stripe/stripe.service.ts
@@ -11,27 +11,25 @@ export class StripeService {
   });
 
   async createCheckoutSession(items: any[]) {
-    const line_items = items.map((item) => ({
-      price_data: {
-        currency: "pkr", // for Pakistan
-        product_data: {
-          name: item.productTitle,
-          // images: [item.image],
-        },
-        unit_amount: item.price * 100, // Stripe expects paisa
-      },
-      quantity: item.quantity,
-    }));
-
-    const session = await this.stripe.checkout.sessions.create({
-      payment_method_types: ["card"],
-      mode: "payment",
-      line_items,
-      success_url: "https://hoorstudio.netlify.app/success",
-      cancel_url: "https://hoorstudio.netlify.app/cancel",
-    });
-
-    // return { url: session.url, id: session.id };
-    return { id: session.id };
+    // const line_items = items.map((item) => ({
+    //   price_data: {
+    //     currency: "pkr", // for Pakistan
+    //     product_data: {
+    //       name: item.productTitle,
+    //       images: [item.image],
+    //     },
+    //     unit_amount: item.price * 100, // Stripe expects paisa
+    //   },
+    //   quantity: item.quantity,
+    // }));
+    // const session = await this.stripe.checkout.sessions.create({
+    //   payment_method_types: ["card"],
+    //   mode: "payment",
+    //   line_items,
+    //   success_url: "https://hoorstudio.netlify.app/success",
+    //   cancel_url: "https://hoorstudio.netlify.app/cancel",
+    // });
+    // // return { url: session.url, id: session.id };
+    // return { id: session.id };
   }
 }
